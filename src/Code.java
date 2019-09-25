5226003660cbc102c4290cd900d4149f3b63aff9
Vivekbhardwaj17 <48515224+Vivekbhardwaj17@users.noreply.github.com>
2019-09-25 19:38:56 +0530

    Update Code.java

--- src/Code.java
+++ src/Code.java
@@ -1,7 +1,7 @@
 public class Code
 {
     public static void main(String[] args) {
-        int[] arr={1,2,3};
+        int[] arr={1,2,3,4,5,6,7,8,9};
         while(arr.length!=1){
             int j=0;
             int[]brr=new int[(arr.length/2)];
